824f02c8ad8c29b553e3a901d61cb495a83db730
pky99 <yadavpraveenjii1999@gmail.com>
2021-01-12 15:31:37 +0530

    connecting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import Avatar from './Avatar';
+import App from './App';
+//import Avatar from './Avatar';
 import reportWebVitals from './reportWebVitals';
 //import Demo from './Demo';
-ReactDOM.render(
+ReactDOM.render(<App />, document.getElementById('root'));
+/*ReactDOM.render(
   <div><Avatar/>
   <Avatar/>
   <Avatar/>
@@ -13,7 +14,7 @@ ReactDOM.render(
   ,document.getElementById('root')
     
 );
-
+*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
